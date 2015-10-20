d575c9e13abb3ab61b3ce43b016c04c32bdbfc1f
zalford <zalford@matthew.cse.taylor.edu>
2015-10-20 11:01:05 -0400

    Refactored Translator.java, updated README

--- README.txt
+++ README.txt
@@ -1,16 +1,23 @@
 Project by: Noah Drabinsky, Zach Alford, Sam Bolds
 
-Included in the submission file is the paper, an example file in our language (House.txt), 
+Included in this project is an example file in our language (House.txt), 
 our grammer (cTurtle2.g4), the Translator files we wrote (Translator.stg and Translator.java),
-example output (House.py), Screenshots for this example (Screenshot of the house example . docx).
+example output (House.py), and a screenshot for this example (Screenshot of the house example . docx).
 Finally, the files that are necessary to use our language are in the folder "FolderContainingFilesForExecution";
 this folder contains the java files necessary after compilation, where Translator.java contains the main() function of the program.
 
+Tunnel Vision Laboratories’ ANTLRWorks 2 software (https://github.com/tunnelvisionlabs/antlrworks2) generates a Java lexer and parser 
+for the grammar, as well as a parse tree listener.
 
-Instructions to use and compile cTurtle2
-
+Instructions to compile and use cTurtle2
 1. Write a file in our language
-2. at command line type "javac *.java"
-3. In directory with the language's parser and translator files, run "java Translator < inputfile.txt > outfile.py"
-4. run outfile.py
-5. Watch turtle be drawn 
+2. Optional step to compile: (within FolderContainingFilesForExecution, the files are already compiled, but if you wish to edit the Java files, you must recompile) run at command line "javac *.java" (if necessary, first install ANTLRWorks 2 software above for the required modules)
+3. (within FolderContainingFilesForExecution) run at command line "java Translator < inputfile.txt > outfile.py"
+4. run outfile.py (you must have the cTurtle module installed (see https://www.cs.ucsb.edu/~pconrad/cs8/topics/cTurtle/))
+5. Watch the drawing 
+
+
+(Zach Alford) For the project to continue, it needs refactoring, in order to be more modular. Then, the language could
+be improved to be more concise. The language could also be improved to import drawings from other files, and easily build
+off of these imported drawings.
+
--- Translator.java
+++ Translator.java
@@ -6,9 +6,9 @@ import java.lang.*;
 import java.util.*;
 
 public class Translator extends cTurtle2BaseListener {
-	STGroup group = new STGroupFile("Translator.stg"); // group.getInstanceOf(...) makes the various StringTemplates accessible
+	STGroup group = new STGroupFile("Translator.stg"); // group.getInstanceOf(...) makes the various StringTemplates accessible in the methods below
 	ParseTreeProperty<ST> STMap = new ParseTreeProperty<ST>(); // this Map takes a ctx (a Parse Tree node) as a key, and returns a String Template. The primary way our Translator passes StringTemplates up the Parse Tree is with this Map
-	List<ST> ListOfFunctionSTs = new ArrayList<ST>(); // these two lists were a more efficient way of sending certain String Templates to the exitFile() function (which builds the driving String Template, the outputted Python file)
+	List<ST> ListOfFunctionSTs = new ArrayList<ST>(); // these two lists were a way of sending certain String Templates to the exitFile() function (which builds the driving String Template, the outputted Python file)
 	List<ST> ListOfStatementSTs = new ArrayList<ST>();
 	Map<String, Turtle> TurtleMap = new HashMap<String, Turtle>();// the main use of this and the class below was to make work the "turtle.attribute" functionality of our language. Would have been a more efficient way to store the input, in retrospect.
 
@@ -28,7 +28,7 @@ public class Translator extends cTurtle2BaseListener {
 		public Turtle(String Name) {
 			this.Name = Name;
 		}
-		public String toString() {
+		public String toString() { // for debugging purposes
 			StringBuffer ReturnString = new StringBuffer();
 			ReturnString.append("Turtle's name " + this.Name + "\n");
 			ReturnString.append("Turtle's FillColor " + this.FillColor);
@@ -54,42 +54,38 @@ public class Translator extends cTurtle2BaseListener {
 	}
 
 	/*
-		The remaining functions build String Templates while walking the parse tree. The main functions here are exitFile() and exitAssignStatement(). All of these, except for the main() function below, extend the functions from cTurtle2BaseListener, and use functionality from cTurtle2Parser.
+		The remaining functions (except for main()) build String Templates while walking the parse tree. The main functions here are exitFile() and exitAssignStatement(). All of these, except for the main() function below, extend the functions from cTurtle2BaseListener, and use functionality from cTurtle2Parser.
 
 	*/
 
 	@Override 
-	public void exitFile(cTurtle2Parser.FileContext ctx) { 
+	public void exitFile(cTurtle2Parser.FileContext ctx) { /*
+		The call to println at the end of this function prints out the text of the Python file to main,
+		which is the desired outcome of the program
+		*/
+
 		ST FileStatement = group.getInstanceOf("file");
 		ST MainStatement = group.getInstanceOf("main");
-		/*
-		if (STMap.get(ctx.statement()).getName().compareTo("/function") == 0 ) {
-			FileStatement.add("functions",STMap.get(ctx.statement()));
-		}*/
+
+		//Add the auto-generated functions (one for each drawing statement) to the body of the python file	
 		for (int i = 0; i < ListOfFunctionSTs.size(); i++) {
 			FileStatement.add("functions", ListOfFunctionSTs.get(i));
 		}
 
+		/*Add the auto-generated statements to the body of main(). These statements call the appropriate
+		auto-generated functions 	
+		*/
 		for (int i = 0; i < ListOfStatementSTs.size(); i++) {
 			MainStatement.add("statements", ListOfStatementSTs.get(i).render());
 		}
-		/*
-		ST MoreStatements = STMap.get(ctx.moreStatements());
-		FileStatement.add("functions", MoreStatements.getAttribute("functions").toString());
-		if (MoreStatements.getAttribute("main") != null) {
-			FileStatement.add("main", MoreStatements.getAttribute("main").toString());
-		}*/
+
+		//Add main to the body of the python file
 		FileStatement.add("main", MainStatement.render());
+
 		System.out.println(FileStatement.render());
 	}
 
-	@Override 
-	public void exitAssignStatementWrapper(cTurtle2Parser.AssignStatementWrapperContext ctx) { 
-		ST AssignStatement = STMap.get(ctx.assignStatement());	
-		ListOfFunctionSTs.add(AssignStatement);
-		//STMap.put(ctx, AssignStatement);
-	}
-
+	// exitDrawStatement() and exitDeleteStatement() both add statements to body of main() by exitFile() using ListOfStatementSTs
 	@Override 
 	public void exitDrawStatement(cTurtle2Parser.DrawStatementContext ctx) { 
 		ST DrawStatement = group.getInstanceOf("drawStatement");	
@@ -104,6 +100,99 @@ public class Translator extends cTurtle2BaseListener {
 		ListOfStatementSTs.add(DeleteStatement);
 	}
 
+	/* 2 purposes for exitAssignStatement()
+	Populate and place CurrentTurtle in TurtleMap, for later use in exitDrawStatement()
+	Populate and place newFunction in ListOfFunctionSTs, for later use in exitFile()
+	 */
+	@Override 
+	public void exitAssignStatement(cTurtle2Parser.AssignStatementContext ctx) { 
+		// Because an Assignment Statement is an implicit creation of a turtle, make a Creation Statement also
+		ST CreateStatement = group.getInstanceOf("createStatement");
+		Turtle CurrentTurtle;
+		// Due to functions below, CurrentTurtle  may have certain attributes already
+		if (TurtleMap.get(ctx.VARIABLE().getText()) == null) {
+			CurrentTurtle = new Turtle(ctx.VARIABLE().getText() + 0);
+		} else {
+			CurrentTurtle = TurtleMap.get(ctx.VARIABLE().getText());
+			CurrentTurtle.FunctionCounter++;
+			CurrentTurtle.Name = ctx.VARIABLE().getText() + CurrentTurtle.FunctionCounter;
+		}
+		
+		// Add creation statement for CurrentTurtle to main
+		CreateStatement.add("turtle", CurrentTurtle.Name);
+		ListOfStatementSTs.add(CreateStatement);
+
+		// Build corresponding function for CurrentTurtle
+		ST newFunction = group.getInstanceOf("function");
+		newFunction.add("name",  CurrentTurtle.Name);
+
+		// Statements within CurrentTurtle's function about the object CurrentTurtle draws
+		ST ObjectStatement = STMap.get(ctx.object());
+
+		Boolean isFillColor = false;
+		if (ctx.getChildCount() > 3) { // if there are in fact Descriptor children of Object
+			for (int i = 0 ; i < (ctx.getChildCount()-3); i++) { // add DescriptorStatement python code to CurrentTurtle's function
+
+				// Statements about the descriptor
+				ST DescriptorStatement = STMap.get(ctx.descriptor(i));
+				if ( DescriptorStatement.getName().compareTo("/RectangleSize") == 0 ) { // add data to CurrentTurtle for use higher up the parse tree, and data to the ObjectStatement
+					ObjectStatement.add("length", DescriptorStatement.getAttribute("l").toString());
+					ObjectStatement.add("width", DescriptorStatement.getAttribute("w").toString());
+					CurrentTurtle.RectangleHeight = DescriptorStatement.getAttribute("l").toString();
+					CurrentTurtle.RectangleWidth = DescriptorStatement.getAttribute("w").toString();
+				} else if ( DescriptorStatement.getName().compareTo("/PolygonSideLength") == 0 ) {// add data to CurrentTurtle for use higher up the parse tree, and data to the ObjectStatement
+					ObjectStatement.add("length", DescriptorStatement.render());
+					CurrentTurtle.PolygonSideLength = DescriptorStatement.render();
+				} else if ( DescriptorStatement.getName().compareTo("/Radius") == 0 ) {// add data to CurrentTurtle for use higher up the parse tree, and data to the ObjectStatement
+					ObjectStatement.add("radius", DescriptorStatement.render());
+					CurrentTurtle.Radius = DescriptorStatement.render();
+				} else { //add any data below to newFunction, as such data does not pertain to the ObjectStatement
+					if (STMap.get(ctx.descriptor(i)).getName().compareTo("/FillColor") == 0 ) {
+						isFillColor = true;
+						CurrentTurtle.FillColor = DescriptorStatement.getAttribute("color").toString();
+					}
+					if (STMap.get(ctx.descriptor(i)).getName().compareTo("/LineColor") == 0 ) {
+						CurrentTurtle.LineColor = DescriptorStatement.getAttribute("color").toString();
+					}
+					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/LineWidth") == 0 ) {
+						CurrentTurtle.LineWidth = DescriptorStatement.getAttribute("size").toString();
+					}
+					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/StartingPos") == 0 ) {
+						CurrentTurtle.StartingPosX = DescriptorStatement.getAttribute("x").toString();
+						CurrentTurtle.StartingPosY = DescriptorStatement.getAttribute("y").toString();
+					}
+					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/DrawingSpeedDigit") == 0 ) {
+						CurrentTurtle.DrawingSpeed = DescriptorStatement.getAttribute("speed").toString();
+					}
+					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/DrawingSpeedInstantaneous") == 0 ) {
+						CurrentTurtle.DrawingSpeed = "instantaneous";
+					}
+					newFunction.add("statements", DescriptorStatement.render());
+				}
+			}
+			if (isFillColor) {
+				newFunction.add("statements", "turtle.begin_fill()");	
+				newFunction.add("statements", "turtle.pendown()");	
+				newFunction.add("statements", ObjectStatement.render());
+				newFunction.add("statements", "turtle.penup()");	
+				newFunction.add("statements", "turtle.end_fill()");	
+				isFillColor = false;
+			}
+			else {
+				newFunction.add("statements", ObjectStatement.render());
+			}
+		}
+		else {
+			newFunction.add("statements", ObjectStatement.render());
+		}
+
+		// Place CurrentTurtle in TurtleMap, for later use in exitFile()
+		TurtleMap.put(ctx.VARIABLE().getText(), CurrentTurtle);
+
+		// Place newFunction in ListOfFunctionSTs, for later use in exitFile()
+		ListOfFunctionSTs.add(newFunction);
+	}
+	
 	@Override 
 	public void exitObjectShape(cTurtle2Parser.ObjectShapeContext ctx) {
 		if (ctx.SHAPE().getText().compareTo("circle") == 0) {
@@ -143,7 +232,6 @@ public class Translator extends cTurtle2BaseListener {
 		} else {
 			LineStatement.add("y", STMap.get(ctx.number(1)).render());
 		}
-		//System.out.println(LineStatement.render());
 		STMap.put(ctx, LineStatement);
 	}
 
@@ -159,123 +247,10 @@ public class Translator extends cTurtle2BaseListener {
 	}
 
 	@Override 
-	public void exitAssignStatement(cTurtle2Parser.AssignStatementContext ctx) { 
-		// Because an Assignment Statement is an implicit creation of a turtle, make a Creation Statement also
-		ST CreateStatement = group.getInstanceOf("createStatement");
-		Turtle CurrentTurtle;
-		if (TurtleMap.get(ctx.VARIABLE().getText()) == null) {
-			CurrentTurtle = new Turtle(ctx.VARIABLE().getText() + 0);
-		} else {
-			CurrentTurtle = TurtleMap.get(ctx.VARIABLE().getText());
-			CurrentTurtle.FunctionCounter++;
-			CurrentTurtle.Name = ctx.VARIABLE().getText() + CurrentTurtle.FunctionCounter;
-		}
-		
-		CreateStatement.add("turtle", CurrentTurtle.Name);
-		ListOfStatementSTs.add(CreateStatement);
-		ST newFunction = group.getInstanceOf("function");
-		newFunction.add("name",  CurrentTurtle.Name);
-		ST ObjectStatement = STMap.get(ctx.object());
-		Boolean isFillColor = false;
-
-		if (ctx.getChildCount() > 3) { // if there are in fact Descriptor children
-			for (int i = 0 ; i < (ctx.getChildCount()-3); i++) {
-				ST DescriptorStatement = STMap.get(ctx.descriptor(i));
-				if ( DescriptorStatement.getName().compareTo("/RectangleSize") == 0 ) {
-					ObjectStatement.add("length", DescriptorStatement.getAttribute("l").toString());
-					ObjectStatement.add("width", DescriptorStatement.getAttribute("w").toString());
-					CurrentTurtle.RectangleHeight = DescriptorStatement.getAttribute("l").toString();
-					CurrentTurtle.RectangleWidth = DescriptorStatement.getAttribute("w").toString();
-				} else if ( DescriptorStatement.getName().compareTo("/PolygonSideLength") == 0 ) {
-					ObjectStatement.add("length", DescriptorStatement.render());
-					CurrentTurtle.PolygonSideLength = DescriptorStatement.render();
-				} else if ( DescriptorStatement.getName().compareTo("/Radius") == 0 ) {
-					ObjectStatement.add("radius", DescriptorStatement.render());
-					CurrentTurtle.Radius = DescriptorStatement.render();
-				} else {
-					if (STMap.get(ctx.descriptor(i)).getName().compareTo("/FillColor") == 0 ) {
-						isFillColor = true;
-						CurrentTurtle.FillColor = DescriptorStatement.getAttribute("color").toString();
-					}
-					if (STMap.get(ctx.descriptor(i)).getName().compareTo("/LineColor") == 0 ) {
-						CurrentTurtle.LineColor = DescriptorStatement.getAttribute("color").toString();
-					}
-					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/LineWidth") == 0 ) {
-						CurrentTurtle.LineWidth = DescriptorStatement.getAttribute("size").toString();
-					}
-					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/StartingPos") == 0 ) {
-						CurrentTurtle.StartingPosX = DescriptorStatement.getAttribute("x").toString();
-						CurrentTurtle.StartingPosY = DescriptorStatement.getAttribute("y").toString();
-					}
-					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/DrawingSpeedDigit") == 0 ) {
-						CurrentTurtle.DrawingSpeed = DescriptorStatement.getAttribute("speed").toString();
-					}
-					if ( STMap.get(ctx.descriptor(i)).getName().compareTo("/DrawingSpeedInstantaneous") == 0 ) {
-						CurrentTurtle.DrawingSpeed = "instantaneous";
-					}
-					newFunction.add("statements", DescriptorStatement.render());
-				}
-			}
-			if (isFillColor) {
-				newFunction.add("statements", "turtle.begin_fill()");	
-				newFunction.add("statements", "turtle.pendown()");	
-				newFunction.add("statements", ObjectStatement.render());
-				newFunction.add("statements", "turtle.penup()");	
-				newFunction.add("statements", "turtle.end_fill()");	
-				isFillColor = false;
-			}
-			else {
-				newFunction.add("statements", ObjectStatement.render());
-			}
-		}
-		else {
-			newFunction.add("statements", ObjectStatement.render());
-		}
-		STMap.put(ctx, newFunction);
-		TurtleMap.put(ctx.VARIABLE().getText(), CurrentTurtle);
-
-		/*
-		List<cTurtle2Parser.DescriptorContext> a = ctx.descriptor();
-		System.out.println("a's size: " +  a.size());
-		for (int i = 0; i < a.size(); i++) {
-			System.out.println(STMap.get(a.get(i)).render());
-		}*/
-		/*
-		if ( DescriptorStatement.getName().compareTo("/RectangleSize") == 0 ) {
-			ObjectStatement.add("length", DescriptorStatement.getAttribute("l").toString());
-			ObjectStatement.add("width", DescriptorStatement.getAttribute("w").toString());
-			newFunction.add("statements", ObjectStatement.render());
-			System.out.println(newFunction.render());
-		}
-		String a = newStatement2.getAttribute("l").toString();
-		ST newStatement = group.getInstanceOf("statement");
-		newStatement.add("blank", "blank");
-		newFunction.add("statements", newStatement.render());
-		newFunction.add("statements", newStatement.render());
-		System.out.println(newFunction.render());*/
-	}
-	
-	
-	/*
-	@Override 
-	public void exitDescriptor(cTurtle2Parser.DescriptorContext ctx) { 
-		ST RectangleSizeStatement = STMap.get(ctx.rectangleSize());
-	}
-	*/
-
-	@Override 
 	public void exitRectSize(cTurtle2Parser.RectSizeContext ctx) { 
 		ST RectangleSizeStatement = STMap.get(ctx.rectangleSize());
 		STMap.put(ctx, RectangleSizeStatement);
 	}
-
-	/*
-	@Override 
-	public void exitFillColor(cTurtle2Parser.FillColorContext ctx) { 
-		
-
-	}
-	*/
 
 	@Override 
 	public void exitStartingPosition(cTurtle2Parser.StartingPositionContext ctx) {
@@ -296,7 +271,6 @@ public class Translator extends cTurtle2BaseListener {
 		ST RectangleSizeStatement = group.getInstanceOf("RectangleSize");
 		RectangleSizeStatement.add("l", STMap.get(ctx.number(0)).render());
 		RectangleSizeStatement.add("w", STMap.get(ctx.number(1)).render());
-		//System.out.println(RectangleSizeStatement.render() + "\n");
 		STMap.put(ctx, RectangleSizeStatement);
 	}
 
@@ -320,9 +294,7 @@ public class Translator extends cTurtle2BaseListener {
 		}
 		ST numberStatement = group.getInstanceOf("Number");
 		numberStatement.add("n", Number.toString());
-		//System.out.println(Number.toString() + "\n");
 		STMap.put(ctx, numberStatement);
-
 	}
 
 	@Override 
@@ -435,7 +407,6 @@ public class Translator extends cTurtle2BaseListener {
         
         ParseTree tree = parser.file();
         
-        //System.out.println(tree.toStringTree(parser));
     }
 }
 
